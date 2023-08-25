ba16e2792716ab49a28a30d0700aecc56583bab7
meduardacarvalho <126161075+meduardacarvalho@users.noreply.github.com>
2023-08-25 18:47:06 -0300

    questao10

---
+++ questao10lista05.java
@@ -0,0 +1,12 @@
+public class questao10lista05 {
+    public static void main(String[] args) {
+    {
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.println("9 x " + i + " = " + (9 * i));
+            i++;
+        }
+    }
+}
+}
